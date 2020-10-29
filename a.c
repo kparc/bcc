f211841b5eae744e80d7f2817f4a110fded146ae
/dev/kelas <me@kel.as>
2020-10-29 06:43:51 +0100

    [+] typesystem 101, misc inline clues

--- a.c
+++ a.c
@@ -25,6 +25,7 @@ ZC b[24];ZS ng(S s){R*--s='-',s;}
 ZS pu(S s,J i){J j;do*--s='0'+i-10*(j=i/10);W(i=j);R s;}
 ZF x(I n){F e=1;N(n,e*=10)R e;}
 S pi(J i){R 0>i?ng(pi(-i)):pu(b+23,i);} //P(NI==f||0>(j|k),nf)
+//! \fixme current state of the art is ryu
 S pf(F f){P(0>f,ng(pf(-f)))P(!f,"0.0")I n=6;W(f<1e6)--n,f*=10;W(f>=1e7)++n,f/=10;S p=n?p=pi(n),*--p='e',p:b+23;n=f+.5;W(99<n&&!(n%10))n/=10;R p=pu(p,n),p[-1]=*p,*p--='.',p;}
 J ip(S p,I n){P('-'==*p,-ip(p+1,n-1))J j=0;N(n,j=10*j+p[i]-'0')R j;}ZS hh(S s,C c){N(2,C a=i?c&15:c>>4;s[i]="0W"[9<a]+a)R s;}S px(J j){S s=b+23;unsigned long k=j;do hh(s-=2,k);W(k>>=8);R s;}
 F fp(S p,I n){P('-'==*p,-fp(p+1,n-1))I l=scn(p,'e',n),m=scn(p,'.',l),f=l<n?ip(p+l+1,n-l-1):0,j=ip(p,m),k;m+=m<l,k=ip(p+m,MN(9,l-=m));k+=j*x(l),f-=l;R 0>f?k/x(-f):k*x(f);} 
@@ -48,11 +49,14 @@ ZI clzl(I n){I i=0;W(n)n/=2,++i;R i-4;}V csr(){R;}//<! \fixme tcc ldmxcsr nyi
 #endif
 
 // malloc free:  mturnnnn list join (k[cifs] k[CIFS])
-ZK M[31];
+//! mturnnnn is K: membucket, type, flags, refcount, payload
+ZK M[31];//!< malloc is a classic pow2 buddy allocator w/o coalescing
 ZK m1(J n){K x,r;I i=clzl(n+7),j;P(x=M[i],M[i]=xx,x)j=i;
  W(!(x=++j<31?M[j]:8+ma(0,16L<<(j=MX(18,i)))));
  xm=i,M[j]=xx,r=x;W(i<j)x+=16L<<xm,M[*(J*)(x-8)=i++]=x,xx=0;R r;}
 
+//! \nt native type widths of Kchijefs: arr, int8, int16, int32, int64, eh?, float, symbol
+//      K c h i j e f s
 J nt[]={8,1,2,4,8,4,8,4};ZJ W=-32;ZV l0();V1(r0){if(Ax||!x)R;if(8==xt){l0(x);R;}if(xr){--xr;R;}if(!xt||KS<xt)N1(xn,r0(Xx))W-=16L<<xm,xx=M[xm],M[xm]=x;}K1(r1){P(Ax,x)R++xr?x:AB("r1");}
 K tn(I t,I n){K x=m1(MX(1,n)*nt[KS<t?0:t]);R W+=16L<<xm,xu=0,xt=t,xn=n,x;}
 ZK xiy(K x,I i,K y){memcpy(x+NX*i,y,NX*yn);if(!yt)N(yn,r1(Yx))R Y0(x);}
@@ -63,7 +67,8 @@ ZK1(l2){R kp("[]");}
 ZV1(l0){if((J)xy)l0(xy),l0(xz);xx=M[xm],M[xm]=x;}
 K3(l1){K r=m1(24);R rt=8,rn=3,rx=x,ry=y,rz=z,r;}
 
-//! repl \ltwvf daz ftz fixme hello segv \see julia
+//! repl daz ftz \lvwtf: \l load \v vars \w wssize \t[:n] timer[ntimes] \f funs
+//! \G global namespace \fixme 1char literals are tough on the ego
 K G[26],ev(),ps();V dis(K,I);ZK es();ZK K0;K k0(){R r1(K0);}ZK vf(I f){K r=kS(0);N(26,K x=G[i];if(NL-x)if(f^(Ax||xt))r=j2(r,ks(i)))R r;}
 ZK Li(K x,I i){R!xt||KS<xt?Xx:KC==xt?kc(Xc):KI==xt?ki(Xi):ks(Xi);}ZK e1(K(*f)(),K x){K r=kK(xn);N1(rn,Rx=f?f(Li(x,i)):Li(x,i))R r;}
 K sS(I c,K x){I n=c?xn:0;N(xn,K y=Xx;n+=yn)K r=kC(n);if(c)--rn;S s=r;N(xn,K y=Xx;s=memcpy(s,y,yn)+yn;if(c)*s++=c)R X0(r);}
@@ -77,7 +82,7 @@ ZK1(se){
   P(!xn,c2("!("[!t],")  0   `"[t]))
   x=sS(";      "[t],e1(se,x));
   R!t?cj('(',jc(x,')')):x;}
-#define DIS 0
+#define DIS 0 //!< \fixme DIS condition is too crude to detect (source;opcodes) tuple
 K1(o){I d=DIS?!Ax&&2==xn&&3==xyu:0;K y=QQ-Ax?d?cj(' ',r1(xx)):se(x):j2(kp((K)"err: "),kp((S)(-1UL>>16&(J)x)));Y0(write(2,y,yn)),nl();if(d)dis(xy,0);R x;}
 ZK1(pr){if(NL-x)r0(o(x));R x;}
 ZS1(ld){J n;Q(s=mf(s,&n))S t=s,u;I a,d=0;W(t<s+n&&d>=0){u=sc(t,10),*u++=0,a=t[1]?0:(*t=='/')-(*t=='\\');if(!d&&!a&&'/'-*t)Q(pr(es(t)))d+=a,t=u;}if(n)munmap(s,n);R NL;}
