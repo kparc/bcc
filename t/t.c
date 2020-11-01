625aa4b8fb6405fb9cba3a1f3449895e974c6e93
kelas <me@kel.as>
2020-11-01 22:45:34 +0100

    [+] add malloc sanity tests

--- t/t.c
+++ t/t.c
@@ -6,7 +6,7 @@ UNIT(smoke,//<! basic sanity
    K x=enm(ki(10)), //!< !10
      y=Li(x,5);     //!< list item
 
-   WS(        64, "workspace size should be 64 bytes")
+   WS(        64, "enm(10) should allocate 64 bytes")
 
    _(Ax,       0, "x should not be an atom")
    _(xr,       0, "x should have refcount 0")
@@ -19,25 +19,32 @@ UNIT(smoke,//<! basic sanity
    _("2+2",    4,   "basic ex #1")
    _("2=2",    1,   "basic ex #2")
 
-   WS(        64, "workspace size should still be 64 bytes")
+   WS(                64, "non-assigning repl expressions shouldn't leak ram")
 
-   _("f[ii]{x+y}",    NONE, "basic ex #3")
-   _("f[40;2]",       42,   "basic ex #4")
+   _("f[ii]{x+y}",  NONE, "declare a global function")
+   WS(               144, "workspace size should be 144 bytes")
 
-   WS(       144, "workspace size should be 144 bytes")
+   _("f[40;2]",       42, "function call should work as expected")
+   WS(               144, "function call shouldn't leak memory")
 
-   r0(x);WS(  80, "r0(x) should return memory to the heap")
+   _("f[ii]{x-y}",  NONE, "reassign an exisiting function")
+   WS(               144, "reassigning a global shouldn't leak memory")
 
-   W0=ws();  //ignore wssize check
+   _("f[40;2]",       38, "reassigned function should quack")
+
+   r0(x);WS(          80, "r0(x) should return memory to the heap")
+
+   W0=ws();  //ignore wssize check FIXME implement global var release
 
    //_("x", expressioR_VALUE, "basic eval should be sane #3")//!< todo
 )
 
 UNIT(parser,//<! parse trees
- PT("#x",    "(\"#\";\"x\")",            "ptree of basic monadic op")
+
+ PT("#x",    "(\"#\";\"x\")\x01",        "ptree of basic monadic op")
  WS(         0,                          "PT() shouldn't leak memory #1")
 
- PT("2*x",   "(\"\\\";\"x\")",           "2*x should translate to \\x")
+ PT("2*x",   "(\"\\\";\"x\")\x01",       "2*x should translate to \\x")
  PT("40+2",  "(\"+\";0xa8;0x82)",        "simple inline expression")
  WS(         0,                          "PT() shouldn't leak memory #2")
 
