cf778f887e9e68bb3b7bc92145b876d2c2029cc5
kelas <me@kel.as>
2020-11-04 18:07:22 +0100

    [~]

--- m.c
+++ m.c
@@ -8,7 +8,7 @@ S ma(I d,J n){ZJ p=BASE;p+=d?0:n;V*r=mmap((V*)(d?0:p-n),n,PROT_READ|PROT_WRITE|P
 S mf(S s,J*n){struct stat b;I d=open(s,0);Qs(0>d,s)R fstat(d,&b),s=(*n=b.st_size)?ma(d,*n):s,close(d),s;}
 K m1(J n){K x,r;I i=clzl(n+7),j;
     P((x=M[i]),M[i]=xx,x)j=i;
-    W(!(x=++j<31?M[j]:8+ma(0,16L<<(j=MX(18,i))))){};
+    W(!(x=++j<31?M[j]:8+ma(0,16L<<(j=MX(18,i))))){}
     xm=i,M[j]=xx,r=x;
     W(i<j)x+=16L<<xm,M[*(J*)(x-8)=i++]=x,xx=0;R r;}
 V1(l0){if((J)xy)l0(xy),l0(xz);xx=M[xm],M[xm]=x;}K3(l1){K r=m1(24);R rt=8,rn=3,rx=x,ry=y,rz=z,r;}K1(l2){R kp("[]");}//!< struct/fixedarray experiment
