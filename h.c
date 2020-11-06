33dfca3eea401f3cbd020382a816fc9a9dfa0910
kelas <me@kel.as>
2020-11-06 08:48:02 +0100

    [+++] HT t=hnew(..) is a proper object

--- h.c
+++ h.c
@@ -6,58 +6,66 @@
 #define SZ sizeof
 #define rea realloc
 #define hmap(h,rng) (h&((rng)-1)) //!< map hash value onto given range (h mod rng-1)
+#define spl t->spl
+#define lvl t->lvl
+#define rds t->rds
+#define mem t->mem
+#define cnt t->cnt
+#define bkt t->bkt
 
+/*
 static B*ht;    //<! hash table
 ZI rds;         //<! balancer rounds
 ZI lvl,spl;     //<! current size of the table divided by 2, split position (initially 0)
 ZJ S1,j;        //!< memory usage, bucket count
+*/
 
 //! djb2 \see www.burtleburtle.net/bob/hash/doobs.html groups.google.com/forum/#!topic/comp.lang.c/lSKWXiuNOAk
 Z_ UI djb(S s,SZT n){UI h=5381;N(n,h=(h<<5)+h+*s++)R h;}//Z_ UI djb(S x,UI n){UI h=5381;N(n,h=33*(h^x[i]));R h;}
 ZV hcpy(V*d,V*s,SZT n){*((S)memcpy(d,s,n)+n)=0;}//!< copy and terminate
-B*hnew(I l,I r){rds=r,lvl=l;ht=(B*)calloc(2*lvl,SZ(B*));R ht;}
-J hmem(){R S1;}J hcnt(){R j;}ZV hbal();
+HT hnew(S id,I l,I r){HT t=(HT)calloc(1,SZHT);I n;t->id=(S)malloc((n=strlen(id))+1);hcpy(t->id,id,n);rds=r,lvl=l,bkt=(B*)calloc(2*l,SZ(B*));R t;}
+ZV hbal(HT t);
 
-S hget(S s,I n){                      //!< lookup or insert string s of length n
+B hget(HT t,S s,I n){                 //!< lookup or insert string s of length n
  B b;UI h=djb(s,n),hi=0,              //!< b bucket, h hash, hi use full range (dbg)
  idx=hmap(h,lvl);                     //!< first, try to map hash value to the 1st half of table
  $(idx<spl,hi=1,idx=hmap(h,lvl<<1));  //!< if idx is above split position, remap to the entire table
- b=ht[idx];                           //!< retrieve the bucket from the slot idx
+ b=bkt[idx];                          //!< retrieve the bucket from the slot idx
  W(b){                                //!< traverse its linked list, if any:
   P(b->n==n,                          //!< if length matches..
    N(n,$(b->s[i]!=s[i],goto L0);)     //!< compare strings char by char
-   b->s)                              //!< if they match, return the existing string
+   b)                                 //!< if they match, return the bucket
   L0:b=b->next;}                      //!< otherwise keep traversing the list.
  I m=SZ(pbkt)+n+1;b=(B)malloc(m);     //!< allocate memory for a new bucket (header size + strlen + terminator)
- S1+=m;b->h=h;b->n=n;                 //!< increment mem usage and initialize bucket header
+ mem+=m;b->h=h;b->n=n;                //!< increment mem usage and initialize bucket header
  hcpy(b->s,s,n);                      //!< copy string to the bucket and terminate it
- b->next=ht[idx];ht[idx]=b;           //!< push the new bucket to the head of the slot
- if(b->next)hbal();                   //!< if slot has a tail, try to split it
- R j++,b->s;}
+ b->next=bkt[idx];bkt[idx]=b;         //!< push the new bucket to the head of the slot
+ if(b->next)hbal(t);                  //!< if slot has a tail, try to split it
+ R cnt++,b;}
 
-ZV hbal(){B*bp,mov;                   //!< balance hash table load
+ZV hbal(HT t){B*bp,mov;               //!< balance hash table load
   N(rds,                              //!< perform rds balancing attempts:
-   bp=&ht[spl];                       //!< retrieve the head bucket at spl
+   bp=&bkt[spl];                      //!< retrieve the head bucket at spl
    I topidx=lvl+spl;                  //!< index of a slot in the upper part
    W(*bp){                            //!< while the slot at spl is not empty
     $(hmap((*bp)->h,lvl<<1)==topidx,  //!< if the bucket's hash maps to topidx
      mov=*bp;*bp=(*bp)->next;         //!< remove it from the list at spl
-     mov->next=ht[topidx];            //!< link the head bucket at newidx to it
-     ht[topidx]=mov)//else            //!< and make it the new head
+     mov->next=bkt[topidx];           //!< link the head bucket at newidx to it
+     bkt[topidx]=mov)//else           //!< and make it the new head
      bp=&(*bp)->next;}                //!< keep traversing the list.
    $(++spl==lvl,                      //!< if ++spl+lvl reaches the size of the table
     lvl<<=1;spl=0;                    //!< then double lvl and reset spl to zero
-    ht=(B*)rea((V*)ht,SZ(B*)*2*lvl);  //!< double the size of the table
-    N(lvl,ht[lvl+i]=0));              //!< zero out the upper part.
+    bkt=(B*)rea((V*)bkt,SZ(B*)*2*lvl);//!< double the size of the table
+    N(lvl,bkt[lvl+i]=0));               //!< zero out the upper part.
   )}
 
 #ifdef TST
-I hslot(){I r=0;N(lvl<<1,r+=!!ht[i])R r;}//<! count occupied slots
-F hload(){R(F)hslot()/j;}
+I hslot(HT t){I r=0;N(lvl<<1,r+=!!bkt[i])R r;}//<! count occupied slots
+F hload(HT t){R(F)hslot(t)/cnt;}
 
-UJ hchk(C o,C st){B b;I n,len,LEN=0,CNT=0;UJ csum,CSUM=0;
+UJ hchk(HT t,C o,C st){B b;I n,len,LEN=0,CNT=0;UJ csum,CSUM=0;
   N(lvl<<1,
-    b=ht[i];n=len=csum=0;
+    b=bkt[i];n=len=csum=0;
     if(o)O("ht[%d]: ",i);
     W(b){
       if(o)O("(%s) -> ",b->s);
--- h.h
+++ h.h
@@ -7,14 +7,25 @@ typedef size_t SZT;
 typedef struct pbkt pbkt;typedef pbkt*B;
 typedef struct pbkt{I h,n;B next;C s[];}pbkt;//!< hash,strlen,next,str+nullchar
 
-B*hnew(I lvl,I rds);     //!< init \p lvl initial size (power of 2) \p rds rounds of tail split attempts
-S hget(S s,I n);         //!< insert|lookup \p s string \p n length
-J hmem(),hcnt();         //!< ram usage, count
+typedef struct ht{
+    S       id;          //< table id
+    I       spl;         //< split position
+    I       lvl;         //< capacity is 2*lvl
+    short   rds;         //< split rounds
+    SZT     cnt;         //< bucket count
+    SZT     mem;         //< mem usage
+    B*      bkt;         //< array of bucket pointers
+    //V*    hea;         //< bucket heap pointer
+} pHT;typedef pHT*HT;
+const static SZT SZHT=sizeof(pHT); //< hash table header size
+
+HT hnew(S id,I lvl,I rds);//!< init \p id table identifier \p lvl initial size (power of 2) \p rds rounds of tail split attempts
+B hget(HT t,S s,I n);    //!< insert|lookup \p s string \p n length
 
 #ifdef TST
-I hslot();               //<! #occupied slots
-F hload();               //<! table load factor
-UJ hchk(C prt,C stat);   //<! print and checksums
+I hslot(HT t);           //<! #occupied slots
+F hload(HT t);           //<! table load factor
+UJ hchk(HT t,C prt,C s); //<! print and checksums
 #endif
 
 //:~
