862fe59d015dc7461f87b267442fd32d61797e68
kelas <me@kel.as>
2020-11-08 13:26:12 +0100

    [=] cp() us() uc() ul() at()

--- u.c
+++ u.c
@@ -1,5 +1,8 @@
+#pragma GCC diagnostic ignored "-Wunsequenced"
 #include"c.h"
 #include <stdint.h>
+
+#define ff     (0x3f&*s++)
 #define T(m)   (!((CP)m&c))
 #define uf(b,s) *u=b|(C)(c>>s);
 #define um(i,s) u[i]=0x80|(C)(c>>s);
@@ -8,27 +11,12 @@
 #define U3(c)   0xe0==(0xf0&c)
 #define U2(c)   0xc0==(0xe0&c)
 
-
-S cp(S s,CP*cp){ //! set cp to the first codepoint in s and return the address of the next
- $(U4(*s),*cp=((0x07&*s)<<18)|((0x3f&s[1])<<12)|((0x3f&s[2])<<6)|(0x3f&s[3]),s+=4)
- $(U3(*s),*cp=((0x0f&*s)<<12)|((0x3f&s[1])<<6)|(0x3f&s[2]),s+=3)
- $(U2(*s),*cp=((0x1f&*s)<<6)|(0x3f&s[1]),s+=2)*cp=*s,s++;R s;}
-
-//! find position of utf needle in utf haystack
-S us(S h,S n){CP t;S m,s;P(!*n,h)W(*h){m=h;s=n;W(*h==*s&&(*h&&*s))h++,s++;P(!*s,m)h=cp(m,&t);}R 0;}
-
-//! find first match of codepoint c in the utf string s
-S uc(S s,CP c){C u[5]={0,0,0,0,0};P(!c,W(*s)s++;s)
- $(T(0xffffff80),*u=(C)c)
- $(T(0xfffff800),uf(0xc0,6)ut(1))
- $(T(0xffff0000),uf(0xe0,12)um(1,6)ut(2))
- $(T(0xffff0000),uf(0xf0,18)um(1,12)um(2,6)ut(3));
- R us(s,u);}
-
-//! count codepoints in utf string s excluding the terminal byte
-UI ul(S s){UI n=0;W(*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;n++;}R n;}
-
-//! return codepoint at index i in s
-CP at(S s,UI i){CP r;UI n=0;W(i-n++&&*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;}R cp(s,&r),r;}
+//! set cp to the first codepoint in s and return the address of the next cp
+S cp(S s,CP*cp){ $(U4(*s),*cp=(0x07&*s++)<<18|ff<<12|ff<<6|ff)$(U3(*s),*cp=(0x0f&*s++)<<12|ff<<6|ff)$(U2(*s),*cp=(0x1f&*s++)<< 6|ff)*cp=*s++;R s;}
+S us(S h,S n){CP t;S m,s;P(!*n,h)W(*h){m=h;s=n;W(*h==*s&&(*h&&*s))h++,s++;P(!*s,m)h=cp(m,&t);}R 0;}//! position of utf needle in utf haystack
+S uc(S s,CP c){C u[5]={0,0,0,0,0};P(!c,W(*s)s++;s)$(T(0xffffff80),*u=(C)c)//! find first match of codepoint c in the utf string s
+ $(T(0xfffff800),uf(0xc0,6)ut(1))$(T(0xffff0000),uf(0xe0,12)um(1,6)ut(2))$(T(0xffff0000),uf(0xf0,18)um(1,12)um(2,6)ut(3));R us(s,u);}
+UI ul(S s){UI n=0;W(*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;n++;}R n;}//! count codepoints in utf string s excluding the terminal byte
+CP at(S s,UI i){CP r;UI n=0;W(i-n++&&*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;}R cp(s,&r),r;}//! return codepoint at index i in s
 
 //:~
