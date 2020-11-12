d5cd6be3532346e88a7ad795ebb672448d0c0e33
kelas <me@kel.as>
2020-11-12 04:12:58 +0100

    [+] cw() codepoint width in bytes

--- t/t.utf.c
+++ t/t.utf.c
@@ -2,22 +2,25 @@
 
 UNIT(utf,
    CP c0,c1,c2;C*t[]={
-      "a\0",
-      "\xce\x93\0",
-      "\xe1\xbd\xb6\0",
-      "\xf0\x90\x8d\x88\0",
-      "\xf0\x9d\x91\x97\xf0\x9d\x91\x98\xf0\x9d\x91\x99\x20\xd0\x90\xd0\x91\xd0\x92\0"}, //𝑗𝑘𝑙 АБВ
-      *gr="αβγδεζηθικλμνξοπρςστυφχψω",
-      *cy="Мне отмщение, и аз воздам";
+  /*0*/(S)"a\0",
+  /*1*/(S)"\xce\x93\0",
+  /*2*/(S)"\xe1\xbd\xb6\0",
+  /*3*/(S)"\xf0\x90\x8d\x88\0",
+  /*4*/(S)"\xf0\x9d\x91\x97\xf0\x9d\x91\x98\xf0\x9d\x91\x99\x20\xd0\x90\xd0\x91\xd0\x92\0"}, //𝑗𝑘𝑙 АБВ
+       *gr=(S)"αβγδεζηθικλμνξοπρςστυφχψω",
+       *cy=(S)"Мне отмщение, и аз воздам";
          //0123456789012345678901234
-
    S ustr0=(S)"\xce\x9e\xce\xb5\xcf\x83\xce\xba\xce\xb5\xcf\x80\xce\xac\xce\xb6"
               "\xcf\x89\x20\xcf\x84\xe1\xbd\xb4\xce\xbd\x20\xcf\x88\xcf\x85\xcf"
               "\x87\xce\xbf\xcf\x86\xce\xb8\xcf\x8c\xcf\x81\xce\xb1\x20\xce\xb2"
               "\xce\xb4\xce\xb5\xce\xbb\xcf\x85\xce\xb3\xce\xbc\xce\xaf\xce\xb1",
               //i uncover the soul-destroying abhorrence
 
-     ustr1=(S)"イロハニホヘト チリヌルヲ ワカヨタレソ ツネナラム ウヰノオクヤマ ケフコエテ アサキユメミシ ヱヒモセスン";
+     ustr1=(S)"イロハニホヘト チリヌルヲ ワカヨタレソ ツネナラム ウヰノオクヤマ ケフコエテ アサキユメミシ ヱヒモセスン ";
+   CP ustr1cp[]={0x30a4,0x30ed,0x30cf,0x30cb,0x30db,0x30d8,0x30c8,0x20,0x30c1,0x30ea,0x30cc,0x30eb,0x30f2,0x20,
+                 0x30ef,0x30ab,0x30e8,0x30bf,0x30ec,0x30bd,0x20,0x30c4,0x30cd,0x30ca,0x30e9,0x30e0,0x20,0x30a6,
+                 0x30f0,0x30ce,0x30aa,0x30af,0x30e4,0x30de,0x20,0x30b1,0x30d5,0x30b3,0x30a8,0x30c6,0x20,0x30a2,
+                 0x30b5,0x30ad,0x30e6,0x30e1,0x30df,0x30b7,0x20,0x30f1,0x30d2,0x30e2,0x30bb,0x30b9,0x30f3,0x20};
 
    //! ul()
    EQ_I(ul(t[0]),            1,     "single 1-byte codepoint should have length 1")
@@ -28,7 +31,7 @@ UNIT(utf,
    EQ_I(ul(gr),             25,     "test vector length should match expected")
    EQ_I(ul(cy),             25,     "test vector length should match expected")
    EQ_I(ul(ustr0),          33,     "test vector length should match expected")
-   EQ_I(ul(ustr1),          55,     "test vector length should match expected")
+   EQ_I(ul(ustr1),          56,     "test vector length should match expected")
 
    //! at()
    EQ_I(at(cy,0),       0x041c,     "utf indexing test #1") // M
@@ -39,8 +42,8 @@ UNIT(utf,
    EQ_I(at(gr,-100),         0,     "utf indexing test #6") // 0
 
    //! us()
-   EQ_I(us(gr,"λ")-gr,      20,     "position of λ must be 20")
-   EQ_I(us(cy,"дам")-cy,    39,     "position of 'дам' must be 39")
+   EQ_I(us(gr,(S)"λ")-gr,   20,     "position of λ must be 20")
+   EQ_I(us(cy,(S)"дам")-cy, 39,     "position of 'дам' must be 39")
 
    //! cp()
    S r=cp(cy,&c0);r=cp(r,&c1);r=cp(r,&c2);
@@ -52,9 +55,16 @@ UNIT(utf,
    EQ_I(uc(gr,0x03c0)-gr,   30,    "position of π should be 30")
    EQ_I(uc(cy,',')-cy,      23,    "position of comma must be 23")
 
-   //! UC()
-   char*rng="ΑωАяĀɏ∀⋿⌀⍺𝑗";C rcl[]={Ug,Ug,Uc,Uc,Ul,Ul,Um,Um,Ua,Ua,UQ};
-   CP c,i=0;S tp=rng;W((tp=cp(tp,&c))&&c)EQ_I(UC(c),rcl[i++],"unicode range classes should match expected");
+  //! cw()
+  I tvec[7]={4,4,4,1,2,2,2};
+  I n=0;S tp=t[4],last=tp;N(7,tp=cp(tp,&c0);I exp=tp-last;last=tp;TRUE(exp==tvec[i]&&tvec[i]==cw(c0),"codepoint byte width should match expected"))
+
+  //! ul()
+  I LAPS=1000;n=ul(ustr1);UI res=0;TN("ul()",LAPS,N(n,c1=at(ustr1,i);res+=c1==ustr1cp[i]))EQ_I(n,res/LAPS,"ul() implementation should be sane");
+
+  //! UC()
+  char*rng="ΑωАяĀɏ∀⋿⌀⍺𝑗";C rcl[]={Ug,Ug,Uc,Uc,Ul,Ul,Um,Um,Ua,Ua,UQ};
+  CP c,i=0;tp=(S)rng;W((tp=cp(tp,&c))&&c)EQ_I(UC(c),rcl[i++],"unicode range classes should match expected");
 )
 
 //:~
--- u.c
+++ u.c
@@ -1,23 +1,29 @@
 #pragma GCC diagnostic ignored "-Wunsequenced"
 #pragma GCC diagnostic ignored "-Wsequence-point"
 
-#include"c.h"
+#include"a.h"
+//extern I clz(I n);
+Z_ UI clz(UI x){R __builtin_clz(x);}
 
 #define ff      (0x3f&*s++)
 #define M(m)    (!((CP)m&c))
-#define uf(b,s) *u=b|(C)(c>>s);
-#define um(i,s) u[i]=0x80|(C)(c>>s);
-#define ut(i)   u[i]=0x80|(C)(c&0x3f)
+#define uf(b,s) *d=b|(C)(c>>s);
+#define um(i,s) d[i]=0x80|(C)(c>>s);
+#define ux(i,x) d[i]=0x80|(C)((x)&0x3f)
+#define ut(i)   ux(i,c)
+#define ue(b,s) case b:ux(w-(b-1),c>>s);
 #define U4(c)   0xf0==(0xf8&c)
 #define U3(c)   0xe0==(0xf0&c)
 #define U2(c)   0xc0==(0xe0&c)
 
 //! set cp to the first codepoint in s and return the address of the next cp
 S cp(S s,CP*cp){R*cp=U4(*s)?(0x07&*s++)<<18|ff<<12|ff<<6|ff:U3(*s)?(0x0f&*s++)<<12|ff<<6|ff:U2(*s)?(0x1f&*s++)<<6|ff:*s++,s;}
-S us(S h,S n){CP t;S m,s;P(!*n,h)W(*h){m=h;s=n;W(*h==*s&&(*h&&*s))h++,s++;P(!*s,m)h=cp(m,&t);}R 0;}//! position of utf needle in utf haystack
-S uc(S s,CP c){C u[5]={0,0,0,0,0};P(!c,W(*s)s++;s)$(M(0xffffff80),*u=(C)c)//! find first match of codepoint c in the utf string s
- $(M(0xfffff800),uf(0xc0,6)ut(1))$(M(0xffff0000),uf(0xe0,12)um(1,6)ut(2))$(M(0xffff0000),uf(0xf0,18)um(1,12)um(2,6)ut(3));R us(s,u);}
-UI ul(S s){UI n=0;W(*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;n++;}R n;}//! count codepoints in utf string s excluding terminal
-CP at(S s,UI i){CP r=0;UI n=0;W(i-n++&&*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;}R cp(s,&r),r;}//! return codepoint at index i in s
+S us(S h,S n){CP t;S m,s;P(!*n,h)W(*h){m=h;s=n;W(*h==*s&&(*h&&*s))h++,s++;P(!*s,m)h=cp(m,&t);}R 0;}//!< position of utf needle in utf haystack
+S uc(S s,CP c){C d[5]={0,0,0,0,0};P(!c,W(*s)s++;s)$(M(0xffffff80),*d=(C)c)//!< find first match of codepoint c in the utf string s
+ $(M(0xfffff800),uf(0xc0,6)ut(1))$(M(0xffff0000),uf(0xe0,12)um(1,6)ut(2))$(M(0xffff0000),uf(0xf0,18)um(1,12)um(2,6)ut(3));R us(s,d);}
+UI ul(S s){UI n=0;W(*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;n++;}R n;}//!< count codepoints in utf string s excluding terminal
+CP at(S s,UI i){CP r=0;UI n=0;W(i-n++&&*s){$(U4(*s),s+=4)$(U3(*s),s+=3)$(U2(*s),s+=2)s++;}R cp(s,&r),r;}//!< return codepoint at index i in s
+C cw(CP c){P(!p,1)static const C l[32]={1,1,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5,6,6,6,6,6,7};R l[31-clz(c)];}//!< codepoint width in bytes
+//V uen(CP c,S d){C w;S(w=cw(c),ue(7,30)ue(6,24)ue(5,18)ue(4,12)ue(3,6)ue(2,0)*d=(0xFF00uL>>w)|(c>>(6*w-6));break;C(1,*d=(C)c));R w;}//!< encode cp into buffer d (<=7 bytes)
 
 //:~
